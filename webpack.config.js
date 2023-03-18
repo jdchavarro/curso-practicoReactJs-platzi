2c1bdd7aa8b832e1a617eb98fd46aa1a58d800a7
jdchavarro <jdchavarro@DESKTOP-RD6GTQ9>
2023-03-17 22:02:01 -0500

    creando primeras rutas con react-router-dom

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/'
     },
     resolve: {
         extensions: ['.js', '.jsx'],
@@ -50,5 +51,6 @@ module.exports = {
         static: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000,
+        historyApiFallback: true,
     }
 }
