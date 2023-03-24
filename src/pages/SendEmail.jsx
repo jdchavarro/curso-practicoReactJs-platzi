7b8186d20ae3f3a6a72c298b35855e1d8196297c
jdchavarro <jdchavarro@DESKTOP-RD6GTQ9>
2023-03-23 22:11:02 -0500

    not found page

--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@styles/SendEmail.scss';
 
 import logo from '@logos/logo_yard_sale.svg'
+import email from '@icons/email.svg';
 
 const SendEmail = () => {
 	return (
@@ -11,7 +12,7 @@ const SendEmail = () => {
 				<h1 className="title">Email has been sent!</h1>
 				<p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
 				<div className="email-image">
-					<img src="./icons/email.svg" alt="email" />
+					<img src={email} alt="email" />
 				</div>
 				<button className="primary-button login-button">Login</button>
 				<p className="resend">
